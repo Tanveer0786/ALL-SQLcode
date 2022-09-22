44ab1e011f5023ef4e8e0bef77003ebc7f1de67d
Tanveer0786 <113006940+Tanveer0786@users.noreply.github.com>
2022-09-22 17:58:39 +0530

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,9 @@
+select * from Account
+select * from branch
+select acnumber,custid from Account
+
+select * from Account where bid is null
+
+update Account set opening_balance='2000'
+where custid='C00001'; opening_balance='1000'
+
